2d926ccc32cafe0fcbc0ff550665d78e5c24f303
Favour <divinephavor@gmail.com>
2023-02-07 16:25:33 +0000

    done task

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a JSON file-writing function"""
+"""
+JSON representation of an object (string)
+"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON format"""
-    with open(filename, "w") as f:
+    """
+    Return JSON repr of an object
+    """
+    with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This module defines a JSON file-reading function"""
+"""
+JSON representation of an object (string)
+"""
 import json
 
 
 def load_from_json_file(filename):
-    """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
+    """
+    Load JSON  object
+    """
+    with open(filename, encoding="utf-8") as f:
         return json.load(f)
